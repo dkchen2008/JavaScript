89810c5e9d38f3978f9d3090cfafacb0e27a531f
dkchen2008 <76940915+dkchen2008@users.noreply.github.com>
2021-03-01 23:31:55 +0800

    Update yuedongzuCOOKIE.js

--- Task/yuedongzuCOOKIE.js
+++ Task/yuedongzuCOOKIE.js
@@ -1,6 +1,6 @@
 //独立COOKIE文件     ck在``里面填写，多账号换行
 
-let yuedongzutokenVal = ``
+let yuedongzutokenVal = `Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wveXVlZG9uZ3p1LnlpY2hlbmd3LmNuXC9hcHBzXC9sb2dpblwvd2VjaGF0IiwiaWF0IjoxNjE0NjA4MTEzLCJleHAiOjIwNTM2MjA4MTEzLCJuYmYiOjE2MTQ2MDgxMTMsImp0aSI6IlJ1RU1McG1FdE9mYU1ycmYiLCJzdWIiOjMwOTY1LCJwcnYiOiI0MWRmODgzNGYxYjk4ZjcwZWZhNjBhYWVkZWY0MjM0MTM3MDA2OTBjIn0.PF-PPLyLR-zvTUPuWSg2HEGCUuKekxzgNaR67GZYuHM`
 
 
 
